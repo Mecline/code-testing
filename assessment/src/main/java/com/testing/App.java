eeefceedc9895368df95f9b33d08e8c127ae3a0e
Maggie Cline <maggie.cline01@gmail.com>
2019-06-10 14:54:19 -0400

    Update App.java

--- assessment/src/main/java/com/testing/App.java
+++ assessment/src/main/java/com/testing/App.java
@@ -9,26 +9,27 @@ public class App
 {
     public static void main( String[] args ) throws IOException, ParseException
     {
-    	
-    	URL CNNurl = new URL ("http://rss.cnn.com/rss/edition.rss");  
-    	URL ESPNurl = new URL("https://www.espn.com/espn/rss/news");
-    	URL washingtonURL = new URL("https://feeds.a.dj.com/rss/RSSWorldNews.xml");
-    	URL meetThePressUrl = new URL("http://podcastfeeds.nbcnews.com/meetthepress");
-    	URL BBCurl = new URL("http://feeds.bbci.co.uk/news/scotland/rss.xml");
-    	URL nasaHurricane= new URL("https://www.nasa.gov/rss/dyn/hurricaneupdate.rss");
-    	
-    	
-    	Map<String, URL> input = new HashMap<String, URL>();
-    	
-    	input.put("CNN", CNNurl);
-    	input.put("Washington Post", washingtonURL);
-    	input.put("Meet the Press", meetThePressUrl);
-    	input.put("BBC News", BBCurl);
-    	input.put("ESPN", ESPNurl);
-    	input.put("NASA Hurricane", nasaHurricane);
         
-        System.out.println(RSSFeeds.readRSSFeed(input, 5));
-//        System.out.println(RSSTesting.readRSSFeed(input, 1));
+        // Main method, uncomment the following to run a test scenario
+    	
+//     	URL CNNurl = new URL ("http://rss.cnn.com/rss/edition.rss");  
+//     	URL ESPNurl = new URL("https://www.espn.com/espn/rss/news");
+//     	URL washingtonURL = new URL("https://feeds.a.dj.com/rss/RSSWorldNews.xml");
+//     	URL meetThePressUrl = new URL("http://podcastfeeds.nbcnews.com/meetthepress");
+//     	URL BBCurl = new URL("http://feeds.bbci.co.uk/news/scotland/rss.xml");
+//     	URL nasaHurricane= new URL("https://www.nasa.gov/rss/dyn/hurricaneupdate.rss");
+    	
+    	
+//     	Map<String, URL> input = new HashMap<String, URL>();
+    	
+//     	input.put("CNN", CNNurl);
+//     	input.put("Washington Post", washingtonURL);
+//     	input.put("Meet the Press", meetThePressUrl);
+//     	input.put("BBC News", BBCurl);
+//     	input.put("ESPN", ESPNurl);
+//     	input.put("NASA Hurricane", nasaHurricane);
+        
+//         System.out.println(RSSFeeds.readRSSFeed(input, 5));
         
 
 
